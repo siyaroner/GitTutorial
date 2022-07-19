ffc378a28686615dc728af7098bb529e3297572e
Siyar Oner <siyaroner@gmail.com>
2022-07-19 11:55:26 +0300

    added some libraries

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 # this is the initial step
 # date: 19.07.2022
 ## Author: Siyar Oner
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
