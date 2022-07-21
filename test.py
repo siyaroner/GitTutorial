d6c261970f0112021f2e46ef16b9eaa6c931215d
Siyar Oner <siyaroner@gmail.com>
2022-07-21 16:34:12 +0300

    add some code

--- test.py
+++ test.py
@@ -6,3 +6,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
+n=2022-1990+1
+data= np.linspace(1990,2022,n,endpoint=True)
